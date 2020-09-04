d2f30c27d4f4755b5c6ae2e26f9e014b66965bf8
hera <a.heraimenka@gmail.com>
2020-09-04 09:43:05 -0700

    Add tasks 1 and 5

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    weights_table = {}
+
+    for i in range(length):
+        weights_table[weights[i]] = i
+
+    for j in range(length):
+        diff = limit - weights[j]
+        
+        if diff in weights_table:
+            if j > weights_table[diff]:
+                return [j, weights_table[diff]]
+            else:
+                return [weights_table[diff], j]
 
     return None
